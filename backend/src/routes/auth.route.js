7fe7605caf5d2b48cfbb77edd2f7bbeb6251f340
sm093615@gmail.com <sm093615@gmail.com>
2026-08-06 14:05:22 +0530

    auth.route.js

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -2,4 +2,16 @@ import express from "express";
 
 
 const router = express.Router();    
+
+router.post("/signup", (req, res) => {
+  res.send("Signup page");
+}); 
+
+router.post("/login", (req, res) => {
+  res.send("Login page");
+}); 
+
+router.post("/logout", (req, res) => {
+  res.send("Logout page");
+}); 
 export default router;  
