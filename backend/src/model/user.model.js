43a63ef032b16ee232756c2343f87c7a5b046c5d
sm093615@gmail.com <sm093615@gmail.com>
2026-08-14 01:39:56 +0530

    update in user.model.js

--- backend/src/model/user.model.js
+++ backend/src/model/user.model.js
@@ -19,7 +19,8 @@ const userSchema = new mongoose.Schema(
          },
          profilePic:{
             type:String,
-            default:"https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
+            default:"",
+            // default:"https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png",
          }
     },
     {timestamps:true}
