aa46ca5db0a1ff630c9bd8da3ef5182bf615b5fc
sm093615@gmail.com <sm093615@gmail.com>
2026-08-16 10:23:10 +0530

    User Email

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,11 +1,17 @@
 /** @format */
-
-export const signup = (req, res) => {
+import User from "../models/user.model.js";
+export const signup = async (req, res) => {
   const {fullName,email,password}=req.body;
   
   try{
     // hash the password
-
+    if(password.length<6){
+      return res.status(400).json({message:"Password must be at least 6 characters long"});
+    }
+    const user = await User.findOne({email});
+      if(user){
+        return res.status(400).json({message:"User Email already exists"});
+      }
   }catch(error){
 
   
