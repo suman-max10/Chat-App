6518795fee60941d2a78f69b08c04a8fe19656ce
sm093615@gmail.com <sm093615@gmail.com>
2026-08-19 13:23:11 +0530

    SignUp Page with include all edge cases

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,5 +1,5 @@
-/** @format */
-import User from "../models/user.model.js";
+import { generateToken } from "../lib/utils.js";
+import User from "../model/user.model.js";
 import bcrypt from "bcryptjs";
 
 
@@ -8,7 +8,9 @@ export const signup = async (req, res) => {
   
   try{
     // hash the password
-
+    if(!fullName || !email || !password){ 
+      return res.status(400).json({message:"Please fill all the fields"});
+    }
     if(password.length<6){
       return res.status(400).json({message:"Password must be at least 6 characters long"});
     }
@@ -22,17 +24,28 @@ export const signup = async (req, res) => {
       const hashedPassword = await bcrypt.hash(password, salt);
 
       const   newUser = new User({
-        fullname,
+        fullname: fullName,
         email,
         password:hashedPassword
       })
       if(newUser){
+        generateToken(newUser._id, res);
+        await newUser.save();
+
+        res.status(201).json({
+          _id: newUser._id,
+          fullname: newUser.fullname,
+          email: newUser.email, 
+          profilePic: newUser.profilePic,
+        });
         
       }else{
          res.status(400).json({message:"Invalid user data"});
 
       }
   }catch(error){
+    console.log("error in signup controller",error.message);
+    res.status(500).json({message:"Server error"}); 
 
   
 }
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 import express from "express";  
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import authRoute from "./routes/auth.route.js"; 
 import {connectDB} from "./lib/db.js";
 dotenv.config();
@@ -7,6 +8,7 @@ const app = express();
 
 const PORT = process.env.PORT ;
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth",authRoute);
 
