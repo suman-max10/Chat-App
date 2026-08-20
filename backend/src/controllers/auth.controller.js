b218a8dd82ffa2b6f0cdad6854f4b2525c643f27
sm093615@gmail.com <sm093615@gmail.com>
2026-08-20 19:36:29 +0530

    Login and logout set with edge cases

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -51,13 +51,45 @@ export const signup = async (req, res) => {
 }
 };
 
-export const login = (req, res) => {
+export const login = async (req, res) => {
+  const {email,password}=req.body;  
+  try{
+    const user = await User.findOne({email});
 
-  res.send("Login page");
+    if(!user){
+      return res.status(400).json({message:"Invalid email or password"});
+    }
 
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if(!isPasswordValid){
+      return res.status(400).json({message:"Invalid email or password"});
+    } 
+
+
+    generateToken(user._id, res);
+
+    res.status(200).json({
+      _id: user._id,
+      fullname: user.fullname,
+      email: user.email, 
+      profilePic: user.profilePic,
+    });
+
+  }catch(error){
+    console.log("error in login controller",error.message);
+    res.status(500).json({message:"Server error"});  
+  }
 };
 
-export const logout = (req, res) => {
-
-  res.send("Logout page");
+export const logout = async (req, res) => {
+  try{
+  res.cookie("jwt","",{maxAge:0});
+  res.status(200).json({message:"Logged out successfully"});
+ }catch(error){
+  console.log("error in logout controller",error.message);
+  res.status(500).json({message:"Server error"});  
+ }  
 };
+
+
+
