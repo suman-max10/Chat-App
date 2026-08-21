3999eda35a0547068e0f56b9d03651b07286f1b8
sm093615@gmail.com <sm093615@gmail.com>
2026-08-21 09:36:59 +0530

    CheckAuth and Protect with postman test

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -116,3 +116,14 @@ export const updateProfile = async (req, res) => {
 
   }
 }
+
+
+export const checkAuth = async (req, res) => {
+  try{
+   res.status(200).json(req.user);
+
+  }catch(error){
+    console.log("error in checkAuth controller",error.message);
+    res.status(500).json({message:"Server error"});
+  }
+}
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,6 +1,6 @@
 import express from "express";
 
-import { signup, login, logout, updateProfile } from "../controllers/auth.controller.js";
+import { signup, login, logout, updateProfile,checkAuth } from "../controllers/auth.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
@@ -12,5 +12,8 @@ router.post("/login", login);
 router.post("/logout", logout);
 
 router.put("/updateProfile", protectRoute, updateProfile);
+
+
+router.get("/check",protectRoute, checkAuth);
 
 export default router;
