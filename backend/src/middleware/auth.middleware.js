b55fe6ed2d8b92d564a2c0107b3f0b03f15dae20
SUMAN MANDAL <128020279+suman-max10@users.noreply.github.com>
2026-08-23 23:56:02 +0530

    Update auth.middleware.js

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -10,6 +10,8 @@ export const protectRoute = async (req, res, next) => {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
 
+
+
         if(!decoded) {
             return res.status(401).json({message:"Unauthorized - Invalid token"});
         }
