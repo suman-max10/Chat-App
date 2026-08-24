7a27a887be35cdef27605a61f53ef477d152ff96
sm093615@gmail.com <sm093615@gmail.com>
2026-08-25 00:09:05 +0530

    Path settings in frontend.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
-
+import NavBar from './components/NavBar';
+import { Routes, Route } from 'react-router-dom';
 const App = () => {
   return (
-    <div className='text-red-500'>
-      <button className='btn'>Button</button>
-      <button className='btn btn-neutral'>Neutral</button>
-      <button className='btn btn-primary'>Primary</button>
-      <button className='btn btn-secondary'>Secondary</button>
-      <button className='btn btn-accent'>Accent</button>
-      <button className='btn btn-ghost'>Ghost</button>
-      <button className='btn btn-link'>Link</button>
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/signup' element={<SignUpPage />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/settings' element={<SettingsPage />} />
+        <Route path='/profile' element={<ProfilePage />} />
+      </Routes>
     </div>
   );
 }
