963e6deaeeb174b07051baae5fc8a785290a7edc
sm093615@gmail.com <sm093615@gmail.com>
2026-08-26 23:57:31 +0530

    init.

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,26 +1,27 @@
-import {useState} from "react";
+/** @format */
+
+import { useState } from "react";
 import { useAuthStore } from "../stores/useAuthStore";
 
 const SignUpPage = () => {
-   const [showPassword, setShowPassword] = useState(false);
-   const[formData,setFormData] = useState({
+  const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState({
     fullName: "",
     email: "",
-    password : "",
-   });
+    password: "",
+  });
 
-   const {signup,isSigningUp} = useAuthStore();
+  const { signup, isSigningUp } = useAuthStore();
 
-   const validateForm = () =>{
+  const validateForm = () => {};
 
-   }
+  const handelSubmit = (e) => {
+    e.preventDefault();
+  };
 
-   const handelSubmit = (e) =>{
-    e.preventDefault()
-    
-   }
-
-  return <div className="min-h-screen grid lg:grid-cols-2"></div>;
+  return <div className='min-h-screen grid lg:grid-cols-2'>
+    {/* {left side } */}
+  </div>;
 };
 
 export default SignUpPage;
