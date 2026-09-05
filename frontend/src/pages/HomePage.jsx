a4e5ac10474558f3272d4e0218727562375d52a6
sm093615@gmail.com <sm093615@gmail.com>
2026-09-05 23:50:49 +0530

    init.

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -3,7 +3,7 @@
 // import Sidebar from "../components/Sidebar";
 // import NoChatSelected from "../components/NoChatSelected";
 // import ChatContainer from "../components/ChatContainer";
-
+// Home Page Content
 const HomePage = () => {
   const { selectedUser } = useChatStore();
 
